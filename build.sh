b48d0e556a796186115bdbe05fc4d400bc91735e
Pangestu Septiansyah <94443224+Pasya22@users.noreply.github.com>
2025-12-14 17:55:58 +0700

    gas

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
-
 echo "Running composer install..."
 composer install --no-dev --optimize-autoloader --no-interaction
 
 echo "Clearing caches..."
 php artisan optimize:clear
+
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "Caching config..."
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
 
 echo "Build complete!"
