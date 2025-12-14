e4ba1234e8f4840e23a74d889452ef6d5c7494cd
Pangestu Septiansyah <94443224+Pasya22@users.noreply.github.com>
2025-12-14 21:47:55 +0700

    CORS

--- config/cors.php
+++ config/cors.php
@@ -22,7 +22,7 @@ return [
     'allowed_origins' => [
         'http://localhost:8000',
         'http://localhost:8080',
-        'https://homielaundry.netlify.app/',
+        'https://homielaundry.netlify.app',
         'http://localhost:3000',
         'http://127.0.0.1:3000',
         'http://localhost:2209',
