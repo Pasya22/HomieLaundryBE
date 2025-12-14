6b665dfffe6a37cba40f59e51d16c08b99f78a5e
Pangestu Septiansyah <94443224+Pasya22@users.noreply.github.com>
2025-12-14 17:15:27 +0700

    try deploy build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,23 @@
+# echo "Running composer install..."
+# composer install --no-dev --optimize-autoloader --no-interaction
+
+# echo "Clearing caches..."
+# php artisan optimize:clear
+
+# echo "Running migrations..."
+# php artisan migrate --force
+
+# echo "Caching config..."
+# php artisan config:cache
+# php artisan route:cache
+# php artisan view:cache
+
+# echo "Build complete!"
+
 echo "Running composer install..."
 composer install --no-dev --optimize-autoloader --no-interaction
 
 echo "Clearing caches..."
 php artisan optimize:clear
-
-echo "Running migrations..."
-php artisan migrate --force
-
-echo "Caching config..."
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
 
 echo "Build complete!"
