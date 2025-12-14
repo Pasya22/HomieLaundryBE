81d3bd1ec453247847be9c80034ef53dfa6c4af2
Pangestu Septiansyah <94443224+Pasya22@users.noreply.github.com>
2025-12-14 23:15:43 +0700

    haayoooo...!!

--- config/cors.php
+++ config/cors.php
@@ -6,13 +6,6 @@ return [
     |--------------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
     |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
     */
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
@@ -29,11 +22,11 @@ return [
         'http://172.168.0.19:8000',
     ],
 
-    'allowed_origins_patterns' => ['*'],
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => ['*'],
+    'exposed_headers' => ['X-CSRF-TOKEN', 'X-XSRF-TOKEN'],
 
     'max_age' => 0,
 
