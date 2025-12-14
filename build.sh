3c228551e2fa14935306ebab6bc84a1f80667fb3
Pangestu Septiansyah <94443224+Pasya22@users.noreply.github.com>
2025-12-14 17:06:08 +0700

    Feat Deploy New

--- build.sh
+++ build.sh
@@ -4,12 +4,12 @@ composer install --no-dev --optimize-autoloader --no-interaction
 echo "Clearing caches..."
 php artisan optimize:clear
 
+echo "Running migrations..."
+php artisan migrate --force
+
 echo "Caching config..."
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-
-echo "Running migrations..."
-php artisan migrate --force
 
 echo "Build complete!"
